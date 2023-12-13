47acd5195bbc55bfb94d3c012cff71c5f3ffb670
wy <1289761536@qq.com>
2023-12-13 22:07:21 +0800

    数据库大作业提交

--- app.py
+++ app.py
@@ -42,10 +42,12 @@ class MovieInfo(db.Model):
     genre = db.Column(db.String(10))
     year = db.Column(db.Integer, nullable=False)
     director = db.Column(db.String(255))  # 导演
+    box_office = db.relationship('MoveBox', backref='movie', uselist=False)
+
 
 class MoveBox(db.Model):
     __tablename__ = 'move_box'
-    movie_id = db.Column(db.String(10), db.ForeignKey('movie_info.movie_id'), primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie_info.movie_id'), primary_key=True)
     box = db.Column(db.Float)
 
 class ActorInfo(db.Model):
@@ -326,98 +328,110 @@ def search_actors():
 
 
 
-
-
 @app.route('/add_movie', methods=['GET', 'POST'])
 def add_movie():
     if request.method == 'POST':
-        # 获取电影信息
+        # 获取电影信息并创建电影实例
         new_movie_name = request.form.get('movie_name')
         new_movie_year = request.form.get('movie_year')
         new_movie_release_date = request.form.get('release_date')
         new_movie_country = request.form.get('country')
         new_movie_genre = request.form.get('genre')
-        new_movie_director = request.form.get('director')
-        new_movie_box_office = request.form.get('box_office')
 
-        # 创建电影实例
+        # 确保获取了所有必要的信息
+        if not all([new_movie_name, new_movie_year, new_movie_release_date, new_movie_country, new_movie_genre]):
+            # 如果有任何一个信息缺失，可以在这里处理错误
+            return "缺少必要的电影信息", 400
+
         new_movie = MovieInfo(
             movie_name=new_movie_name,
             year=int(new_movie_year),
             release_date=datetime.strptime(new_movie_release_date, '%Y-%m-%d'),
             country=new_movie_country,
-            genre=new_movie_genre,
-            director=new_movie_director
+            genre=new_movie_genre
         )
         db.session.add(new_movie)
         db.session.flush()  # 立即生成新电影的ID
 
         # 添加票房信息
+        new_movie_box_office = request.form.get('box_office')
         if new_movie_box_office:
             existing_box_office = MoveBox.query.filter_by(movie_id=new_movie.movie_id).first()
-            if not existing_box_office:
+            if existing_box_office:
+                existing_box_office.box = float(new_movie_box_office)
+            else:
                 new_box_office = MoveBox(
                     movie_id=new_movie.movie_id,
                     box=float(new_movie_box_office)
                 )
                 db.session.add(new_box_office)
 
-        # 处理现有演员和导演
-        existing_actors = request.form.getlist('actors')
-        existing_directors = request.form.getlist('directors')
-        for actor_id in existing_actors:
-            relation = MovieActorRelation(
-                movie_id=new_movie.movie_id,
-                actor_id=actor_id,
-                relation_type='主演'
-            )
-            db.session.add(relation)
-        for director_id in existing_directors:
-            relation = MovieActorRelation(
-                movie_id=new_movie.movie_id,
-                actor_id=director_id,
-                relation_type='导演'
-            )
-            db.session.add(relation)
+        # 处理多个新演员
+        new_actor_names = request.form.getlist('new_actor_name[]')
+        new_actor_genders = request.form.getlist('new_actor_gender[]')
+        new_actor_countries = request.form.getlist('new_actor_country[]')
+        for name, gender, country in zip(new_actor_names, new_actor_genders, new_actor_countries):
+            if name:
+                existing_actor = ActorInfo.query.filter_by(actor_name=name).first()
+                if existing_actor:
+                    # 如果演员已存在，更新信息
+                    existing_actor.gender = gender
+                    existing_actor.country = country
+                    db.session.flush()
+                    new_actor_relation = MovieActorRelation(
+                        movie_id=new_movie.movie_id,
+                        actor_id=existing_actor.actor_id,
+                        relation_type='主演'
+                    )
+                else:
+                    # 如果演员不存在，创建新演员
+                    new_actor = ActorInfo(actor_name=name, gender=gender, country=country)
+                    db.session.add(new_actor)
+                    db.session.flush()
+                    new_actor_relation = MovieActorRelation(
+                        movie_id=new_movie.movie_id,
+                        actor_id=new_actor.actor_id,
+                        relation_type='主演'
+                    )
 
-        # 处理新演员
-        new_actor_name = request.form.get('new_actor_name')
-        if new_actor_name:
-            new_actor = ActorInfo(
-                actor_name=new_actor_name,
-                gender=request.form.get('new_actor_gender'),
-                country=request.form.get('new_actor_country')
-            )
-            db.session.add(new_actor)
-            db.session.flush()  # 立即生成新演员的ID
-            new_actor_relation = MovieActorRelation(
-                movie_id=new_movie.movie_id,
-                actor_id=new_actor.actor_id,
-                relation_type='主演'  # 假设新添加的都是主演
-            )
-            db.session.add(new_actor_relation)
+                db.session.add(new_actor_relation)
 
-        # 处理新导演
-        new_director_name = request.form.get('new_director_name')
-        if new_director_name:
-            new_director = ActorInfo(
-                actor_name=new_director_name,
-                gender=request.form.get('new_director_gender'),
-                country=request.form.get('new_director_country')
-            )
-        db.session.add(new_director)
-        db.session.flush()  # 立即生成新导演的ID
-        new_director_relation = MovieActorRelation(
-            movie_id=new_movie.movie_id,
-            actor_id=new_director.actor_id,
-            relation_type='导演'
-        )
-        db.session.add(new_director_relation)
+        # 处理多个新导演
+        new_director_names = request.form.getlist('new_director_name[]')
+        new_director_genders = request.form.getlist('new_director_gender[]')
+        new_director_countries = request.form.getlist('new_director_country[]')
+        for name, gender, country in zip(new_director_names, new_director_genders, new_director_countries):
+            if name:
+                existing_director = ActorInfo.query.filter_by(actor_name=name).first()
+                if existing_director:
+                    # 如果导演已存在，更新信息
+                    existing_director.gender = gender
+                    existing_director.country = country
+                    db.session.flush()
+                    new_director_relation = MovieActorRelation(
+                        movie_id=new_movie.movie_id,
+                        actor_id=existing_director.actor_id,
+                        relation_type='导演'
+                    )
+                else:
+                    # 如果导演不存在，创建新导演
+                    new_director = ActorInfo(actor_name=name, gender=gender, country=country)
+                    db.session.add(new_director)
+                    db.session.flush()
+                    new_director_relation = MovieActorRelation(
+                        movie_id=new_movie.movie_id,
+                        actor_id=new_director.actor_id,
+                        relation_type='导演'
+                    )
+
+                db.session.add(new_director_relation)
+
         db.session.commit()
         return redirect(url_for('index'))
 
     actors = ActorInfo.query.all()
     return render_template('add_movie.html', actors=actors)
+
 
 
 @app.route('/movie_list')
@@ -445,15 +459,33 @@ def show_movie(movie_id):
 
 @app.route('/delete_movie/<int:movie_id>', methods=['POST'])
 def delete_movie(movie_id):
+    # 获取电影实例
     movie = MovieInfo.query.get_or_404(movie_id)
+
+    # 删除与电影相关的演员和导演关系
+    MovieActorRelation.query.filter_by(movie_id=movie.movie_id).delete()
+
+    # 删除电影的票房信息
+    MoveBox.query.filter_by(movie_id=movie.movie_id).delete()
+
+    # 删除电影本身
     db.session.delete(movie)
+
+    # 提交更改
     db.session.commit()
-    flash('电影已删除')
+
+    flash('电影及相关信息已删除')
     return redirect(url_for('index'))
+
 
 
 @app.route('/box_office_analysis')
 def box_office_analysis():
+    # 获取票房前十的电影
+    top_movies_analysis = db.session.query(
+        MovieInfo.movie_name, func.sum(MoveBox.box)
+    ).join(MoveBox).group_by(MovieInfo.movie_name).order_by(func.sum(MoveBox.box).desc()).limit(10).all()
+
     # 按类型分析票房
     genre_analysis = db.session.query(
         MovieInfo.genre, func.sum(MoveBox.box)
@@ -477,6 +509,7 @@ def box_office_analysis():
 
 
     # 数据转换为前端格式
+    top_movie_names, top_movie_box_offices = zip(*top_movies_analysis)
     genres, genre_box_offices = zip(*genre_analysis)
     countries, country_box_offices = zip(*country_analysis)
     years, year_box_offices = zip(*year_analysis)
@@ -493,9 +526,82 @@ def box_office_analysis():
         year_box_offices=list(year_box_offices),
         movie_names=list(movie_names),
         movie_box_offices=list(movie_box_offices),
+        top_movie_names=list(top_movie_names),
+        top_movie_box_offices=list(top_movie_box_offices),
     )
 
 
+
+# Route to display a list of actors
+@app.route('/actor_list')
+def actor_list():
+    actors = ActorInfo.query.all()
+    return render_template('actor_list.html', actors=actors)
+
+
+# Route to display the details of a specific actor
+@app.route('/show_actor/<int:actor_id>')
+def show_actor(actor_id):
+    actor = ActorInfo.query.get_or_404(actor_id)
+
+    # 获取演员参与的电影作为主演的列表
+    movies_as_actor = MovieInfo.query.join(MovieActorRelation, MovieInfo.movie_id == MovieActorRelation.movie_id)\
+                                     .filter(MovieActorRelation.actor_id == actor.actor_id, MovieActorRelation.relation_type == '主演').all()
+
+    # 获取演员参与的电影作为导演的列表
+    movies_as_director = MovieInfo.query.join(MovieActorRelation, MovieInfo.movie_id == MovieActorRelation.movie_id)\
+                                        .filter(MovieActorRelation.actor_id == actor.actor_id, MovieActorRelation.relation_type == '导演').all()
+
+    return render_template('show_actor.html', actor=actor, movies_as_actor=movies_as_actor, movies_as_director=movies_as_director)
+
+# Route to handle actor deletion
+@app.route('/delete_actor/<int:actor_id>', methods=['POST'])
+def delete_actor(actor_id):
+    actor = ActorInfo.query.get_or_404(actor_id)
+
+    # 删除该演员相关的所有电影演员关系
+    MovieActorRelation.query.filter_by(actor_id=actor.actor_id).delete()
+
+    db.session.delete(actor)
+    db.session.commit()
+    flash('演员删除成功。')
+    return redirect(url_for('actor_list'))
+
+@app.route('/box_office_list')
+def box_office_list():
+    # 获取电影数据并按票房从高到低排序
+    movies = MovieInfo.query.join(MoveBox, MovieInfo.movie_id == MoveBox.movie_id)\
+                            .order_by(MoveBox.box.desc()).all()
+    return render_template('box_office_list.html', movies=movies)
+
+
+
+@app.route('/search_by_genre', methods=['GET', 'POST'])
+def search_by_genre():
+    genres = db.session.query(MovieInfo.genre).distinct().all()  # 获取所有独特的电影类别
+    genre_list = [genre[0] for genre in genres]  # 将类别转换为列表
+
+    selected_genre = request.args.get('genre')  # 从请求中获取选定的类别
+    if selected_genre:
+        movies = MovieInfo.query.filter_by(genre=selected_genre).all()
+    else:
+        movies = []
+
+    return render_template('search_by_genre.html', genres=genre_list, movies=movies, selected_genre=selected_genre)
+
+@app.route('/search_by_year', methods=['GET', 'POST'])
+def search_by_year():
+    years = db.session.query(MovieInfo.year).distinct().all()  # 获取所有独特的电影年份
+    year_list = [year[0] for year in years]  # 将年份转换为列表
+
+    selected_year = request.args.get('year')  # 从请求中获取选定的年份
+    if selected_year:
+        selected_year_int = int(selected_year)  # 将字符串转换为整数
+        movies = MovieInfo.query.filter_by(year=selected_year_int).all()
+    else:
+        movies = []
+
+    return render_template('search_by_year.html', years=year_list, movies=movies, selected_year=selected_year)
 
 
 if __name__ == '__main__':
